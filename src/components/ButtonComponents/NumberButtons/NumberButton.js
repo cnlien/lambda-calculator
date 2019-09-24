444252336b8535c8fe999621851c45a08536b466
Christopher Lien <cnlien@gmail.com>
2019-09-24 16:56:12 -0400

    More Styles

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NumberButton = (props) => {
   return (
-    <button class={ `button` + props.number }>
+    <button class={ `button` + props.number } name={`button`+props.number}>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.number}
     </button>
